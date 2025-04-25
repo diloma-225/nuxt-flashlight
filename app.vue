b62254c0a6ec36c22335f007c0b4f37580fa8d62
diloma225 <dilomapierrekarma@gmail.com>
2025-04-25 17:54:12 +0000

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      {{ test }}
+      <ion-button shape="round" @click="checkFlashlightAvailability()">FLASH LIGHT</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+<script setup>
+import { CapacitorFlash } from '@capgo/capacitor-flash';
+
+const test = ref(null);
+const checkFlashlightAvailability = () => {
+  CapacitorFlash.isAvailable().then((result) => {
+    test.value = result.value;
+  });
+};
+
+</script>
